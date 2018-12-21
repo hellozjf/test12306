662497f04f25811ae208b11e14ba3008ca3100be
hellozjf <908686171@qq.com>
2018-12-21 11:11:08 +0800

    init

---
+++ src/main/java/com/hellozjf/test/test12306/Test12306Application.java
@@ -0,0 +1,14 @@
+package com.hellozjf.test.test12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test12306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test12306Application.class, args);
+    }
+
+}
+
